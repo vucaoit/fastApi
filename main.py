defb17648ac5a15785380d2d84c456b7bf3ccd45
vucaoit <86138777+vucaoit@users.noreply.github.com>
2023-05-29 09:21:42 +0700

    Update main.py

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ from security import SECRET_KEY, SECURITY_ALGORITHM, validate_token, reusable_oa
 from database import SessionLocal, engine
 
 models.Base.metadata.create_all(bind=engine)
+from typing import List
 
 app = FastAPI()
 
@@ -36,7 +37,7 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     return crud.create_user(db=db, user=user)
 
 
-@app.get("/users/", response_model=list[schemas.User])
+@app.get("/users/", response_model=List[schemas.User])
 def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     users = crud.get_users(db, skip=skip, limit=limit)
     return users
@@ -57,7 +58,7 @@ def create_item_for_user(
     return crud.create_user_item(db=db, item=item, user_id=user_id)
 
 
-@app.get("/items/", response_model=list[schemas.Item])
+@app.get("/items/", response_model=List[schemas.Item])
 def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     items = crud.get_items(db, skip=skip, limit=limit)
     return items
